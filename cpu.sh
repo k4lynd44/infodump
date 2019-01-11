9570bb63fae63555bfa547c18d0916f1563c62d2
mariannafucci <mariannafucci@yahoo.it>
2019-01-11 12:12:11 +0100

    Aggiunto script per le misure di prestazioni

---
+++ cpu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+printf "Memory\t\tDisk\t\tCPU\n"
+end=$((SECONDS+3600))
+while [ $SECONDS -lt $end ]; do
+MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
+DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
+CPU=$(top -b -n1 | grep "Cpu(s)" | awk '{print $2"%\t\t"}')
+echo "$MEMORY$DISK$CPU"
+sleep 2
+done
